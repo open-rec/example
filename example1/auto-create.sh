6a31248323b5ab9e11807f283abe3408ed42c967
xsank <xsank@foxmail.com>
2022-11-07 10:33:41 +0800

    update create sh

--- example1/auto-create.sh
+++ example1/auto-create.sh
@@ -1,10 +1,11 @@
+#/bin/bash
 
 # install redis
 brew install redis
 brew services start redis
 
 # init redis data
-# todo
+java -jar rec-example-init-1.0-SNAPSHOT.jar 127.0.0.1 6379
 
 # start rec server
-# todo
+nohup java -jar rec-server-1.0-SNAPSHOT.jar 2>&1 > auto-create.log &
