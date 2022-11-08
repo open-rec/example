5b373f719d212e7497dd49f32031b0e5a1e3ff3e
xsank <xsank@foxmail.com>
2022-11-08 17:30:45 +0800

    add es

--- example1/auto-create.sh
+++ example1/auto-create.sh
@@ -4,6 +4,14 @@
 brew install redis
 brew services start redis
 
+# install elasticsearch
+# for version 8.5.0, elasticsearch access with security limit, create common test user later.
+curl -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.5.0-darwin-x86_64.tar.gz
+tar -xzf elasticsearch-8.5.0-darwin-x86_64.tar.gz
+cd elasticsearch-8.5.0/
+./bin/elasticsearch -d
+
+
 # init redis data
 java -jar rec-example-init-1.0-SNAPSHOT.jar 127.0.0.1 6379
 
